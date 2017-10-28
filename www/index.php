a6b8417fad2c523eefdf4d78fdc3b5b7d0ee7149
Unknown <babathiste@gmail.com>
2017-10-28 08:33:34 +0200

    Added Files

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php
-$dir_nom = '.'; // dossier listé (pour lister le répertoir courant : $dir_nom = '.'  --> ('point')
+$dir_nom = '.'; // dossier listé (pour lister le répertoire courant : $dir_nom = '.'  --> ('point')
 $dir = opendir($dir_nom) or die('Erreur de listage : le répertoire n\'existe pas'); // on ouvre le contenu du dossier courant
 $fichier= array(); // on déclare le tableau contenant le nom des fichiers
 $dossier= array(); // on déclare le tableau contenant le nom des dossiers
@@ -25,11 +25,12 @@ if(!empty($dossier)) {
 
 if(!empty($fichier)){
 	sort($fichier);// pour le tri croissant, rsort() pour le tri décroissant
-	echo "Liste des fichiers accessibles: \n\n";
+	echo "Some links: \n\n";
 	echo "\t\t<ul>\n";
-		foreach($fichier as $lien) {
-			echo "\t\t\t<li><a href=\"$dir_nom/$lien \">$lien</a></li>\n";
-		}
+		
+			echo "\t\t\t<li><a href=\"$dir_nom/link_of_my_application.php \">$lien</a></li>\n";
+			echo "\t\t\t<li><a href=\"$dir_nom/riot.txt \">$lien</a></li>\n";
+		
 	echo "\t\t</ul>";
  }
 ?>
---
+++ www/link_of_my_application.php
@@ -0,0 +1,7 @@
+<?php
+
+
+echo "The link of my application is : \n\nhttps://github.com/Commantary/discord-bot-comabot/blob/heroku/module/lolsearch.js";
+
+
+?>
